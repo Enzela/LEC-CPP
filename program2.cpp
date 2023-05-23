519f6aa39c0551472f8a9779ac6f92f2e1a180ca
Enzela <133377833+Enzela@users.noreply.github.com>
2023-05-23 13:50:23 +0545

    1st chapter

---
+++ program2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class Account{
+    public:
+    float salary=60000;
+
+    Account(){
+
+    cout<<"This is Account";
+    }
+};
+
+
+int main(){
+}
+
+
+
+
+
+
+
